4c35e3b0c3fd0bfdd8176e9a15244025063bf8d0
Sushants-Git <sushantsgml@gmail.com>
2024-02-19 23:30:53 +0530

    Adding jsDoc comments and changed README.js -- V 1.0.2

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
 # vague-finder
+
+`vagueFinder` is a simple and easy to use package for sentences similarity operations. Was mainly created to be used in searching operations.
+
+## Installation
+
+Install `vagueFinder` using npm:
+
+```bash
+npm install vagueFinder
+```
+
+## Usage
+
+### Loading the Model
+
+Before you can compare sentences, you need to load the model. This is an asynchronous operation. The model loaded is small and fast enough for most use cases.
+
+```js
+await vagueFinder.loadModel();
+```
+
+### Tracking Progress
+
+You can get the progress of the model loading process. This returns an Object containing the progress information.
+
+```js
+const progress = vagueFinder.getProgress();
+console.log(progress);
+```
+
+example output : 
+
+```json
+{
+    "status": "progress",
+    "name": "Supabase/gte-small",
+    "file": "onnx/model_quantized.onnx",
+    "progress": 1.5894844146421874,
+    "loaded": 540654,
+    "total": 34014426
+}
+```
+
+### Comparing Two Sentences
+
+To compare two sentences, use the `compareTwoSentences` method. This returns an object containing the two input sentences and the calculated similarity.
+
+```js
+const result = await vagueFinder.compareTwoSentences("This is a sentence.", "This is another sentence.");
+console.log(`The similarity between "${result.sentenceOne}" and "${result.sentenceTwo}" is ${result.similarity}.`);
+```
+
+### Comparing a Sentence to an Array of Sentences
+
+To compare a sentence to an array of sentences, use the `compareSentenceToArray` method. This returns an object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity.
+
+```js
+const result = await vagueFinder.compareSentenceToArray("This is a sentence.", ["This is another sentence.", "Yet another sentence."]);
+result.array.forEach((comparison) => {
+  console.log(`The similarity between "${result.sentence}" and "${comparison.sentence}" is ${comparison.similarity}.`);
+});
+```
+
+### Comparing a Sentence to an Array of Sentences in Order of Similarity
+
+To compare a sentence to an array of sentences and get the results in order of similarity, use the `arrayInOrder` method. This returns an object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity, sorted in descending order of similarity.
+
+```js
+const result = await vagueFinder.arrayInOrder("This is a sentence.", ["This is another sentence.", "Yet another sentence."]);
+result.array.forEach((comparison, index) => {
+  console.log(`#${index + 1}: The similarity between "${result.sentence}" and "${comparison.sentence}" is ${comparison.similarity}.`);
+});
+```
+
+
+## API
+
+`loadModel()`
+
+Asynchronously loads the model. This must be called before any of the comparison methods. Throws an error if the model cannot be loaded.
+
+`getProgress()`
+
+You can get the progress of the model loading process. This returns an Object containing the progress information.
+
+`compareTwoSentences(sentenceOne, sentenceTwo)`
+
+Compares two sentences using the loaded model. Returns an object containing the two input sentences and the calculated similarity. Throws an error if the model has not been loaded.
+
+`compareSentenceToArray(sentence, array)`
+
+Compares a sentence to an array of sentences using the loaded model. Returns an object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity. Throws an error if the model has not been loaded.
+
+`arrayInOrder(sentence, array)`
+
+Compares a sentence to an array of sentences using the loaded model and returns the results in order of similarity. Returns an object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity, sorted in descending order of similarity. Throws an error if the model has not been loaded.
+
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
--- index.js
+++ index.js
@@ -22,24 +22,71 @@ class PipelineSingleton {
 }
 
 let model = null;
-
 let progress = null;
 
+/**
+ * Asynchronously loads the model.
+ *
+ * This function gets the pipeline instance which will load and build the model when run for the first time.
+ * It also provides a way to track the progress of the pipeline creation, which can be used to update a UI element like a progress bar.
+ *
+ * @async
+ * @function
+ * @throws {Error} If the model cannot be loaded, an error is thrown with a message detailing the reason.
+ *
+ * @example
+ * try {
+ *   await loadModel();
+ * } catch (error) {
+ *   console.error(error);
+ * }
+ */
+
 async function loadModel() {
-  // Get the pipeline instance. This will load and build the model when run for the first time.
-  model = await PipelineSingleton.getInstance((data) => {
-    // You can track the progress of the pipeline creation here.
-    // e.g., you can send `data` back to the UI to indicate a progress bar
-    progress = data;
-    // console.log("progress", data);
-  });
+  try {
+    // Get the pipeline instance. This will load and build the model when run for the first time.
+    model = await PipelineSingleton.getInstance((data) => {
+      // You can track the progress of the pipeline creation here.
+      // e.g., you can send `data` back to the UI to indicate a progress bar
+      // can be accessed via vagueFinder.getProgress()
+      progress = data;
+    });
+  } catch (error) {
+    throw new Error(`Unable to load Model due to ${error}`);
+  }
 }
+
+/**
+ * Throws an error indicating that the model has not been loaded.
+ *
+ *
+ * @function
+ * @throws {Error} Always throws an error indicating that the model has not been loaded.
+ */
 
 function modelNotLoadedErrorMessage() {
-  console.log("Model has not been loaded, use vagueFinder.loadModel()");
+  throw new Error("Model has not been loaded, use vagueFinder.loadModel()");
 }
 
+/**
+ * Asynchronously classifies the similarity between two sentences using cosine similarity.
+ *
+ * This function takes two sentences as input, converts them into embeddings using the model, and then calculates the cosine similarity between the embeddings.
+ * The result is an object containing the two input sentences and the calculated similarity.
+ *
+ * @async
+ * @function
+ * @param {string} sentenceOne - The first sentence to compare.
+ * @param {string} sentenceTwo - The second sentence to compare.
+ * @returns {Object} The result object containing the two input sentences and the calculated similarity.
+ * @throws {Error} If the model has not been loaded. 
+*/
+
 const classify = async (sentenceOne, sentenceTwo) => {
+  if (!model) {
+    modelNotLoadedErrorMessage();
+    return;
+  }
   let embedding1 = await model(sentenceOne, {
     pooling: "mean",
     normalize: true,
@@ -60,16 +107,12 @@ const classify = async (sentenceOne, sentenceTwo) => {
   let result = similarity;
 
   function calculateCosineSimilarity(embedding1, embedding2) {
-    // Calculate dot product
+    // Calculate dot product and magnitudes
     let dotProduct = 0;
-    for (let i = 0; i < embedding1.length; i++) {
-      dotProduct += embedding1[i] * embedding2[i];
-    }
-
-    // Calculate magnitudes
     let magnitude1 = 0;
     let magnitude2 = 0;
     for (let i = 0; i < embedding1.length; i++) {
+      dotProduct += embedding1[i] * embedding2[i];
       magnitude1 += embedding1[i] * embedding1[i];
       magnitude2 += embedding2[i] * embedding2[i];
     }
@@ -88,16 +131,37 @@ const classify = async (sentenceOne, sentenceTwo) => {
   };
 };
 
+/**
+ * Asynchronously compares a sentence to an array of sentences using cosine similarity.
+ * 
+ * This function takes a sentence and an array of sentences as input. It compares the input sentence to each sentence in the array using the `classify` function, which calculates the cosine similarity between the sentences.
+ * The result is an object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity to the input sentence.
+ * 
+ * @async
+ * @function
+ * @param {string} sentence - The sentence to compare to the array of sentences.
+ * @param {Array<string>} array - The array of sentences to compare to the input sentence.
+ * @returns {Object} The result object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity.
+ * @throws {Error} If the model has not been loaded.
+ * 
+ * @example
+ * try {
+ *   const result = await compareSentenceToArray("This is a sentence.", ["This is another sentence.", "Yet another sentence."]);
+ *   console.log(result);
+ * } catch (error) {
+ *   console.error(error);
+ * }
+ */
+
 const compareSentenceToArray = async (sentence, array) => {
   if (!model) {
     modelNotLoadedErrorMessage();
     return;
   }
-  array = [...array] //Creating a copy, so that we don't alter the original;
+  array = [...array]; //Creating a copy, so that we don't alter the original;
   for (let i = 0; i < array.length; i++) {
-    await classify(sentence, array[i]).then(({ sentenceTwo, alike }) => {
-      array[i] = { sentenceTwo: sentenceTwo, alike: alike };
-    });
+    const { sentenceTwo, alike } = await classify(sentence, array[i]);
+    array[i] = { sentenceTwo: sentenceTwo, alike: alike };
   }
 
   return {
@@ -106,15 +170,37 @@ const compareSentenceToArray = async (sentence, array) => {
   };
 };
 
+/**
+ * Asynchronously compares a sentence to an array of sentences and returns the results in order of similarity.
+ * 
+ * This function takes a sentence and an array of sentences as input. It compares the input sentence to each sentence in the array using the `compareSentenceToArray` function, which calculates the cosine similarity between the sentences.
+ * The result is an object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity. The array is sorted in descending order of similarity.
+ * 
+ * @async
+ * @function
+ * @param {string} sentence - The sentence to compare to the array of sentences.
+ * @param {Array<string>} array - The array of sentences to compare to the input sentence.
+ * @returns {Object} The result object containing the input sentence and an array of objects, each containing a sentence from the input array and the calculated similarity, sorted in descending order of similarity.
+ * @throws {Error} If the model has not been loaded.
+ * 
+ * @example
+ * try {
+ *   const result = await arrayInOrder("This is a sentence.", ["This is another sentence.", "Yet another sentence."]);
+ *   console.log(result);
+ * } catch (error) {
+ *   console.error(error);
+ * }
+ */
+
 const arrayInOrder = async (sentence, array) => {
   if (!model) {
     modelNotLoadedErrorMessage();
     return;
   }
-  array = [...array] //Creating a copy, so that we don't alter the original;
+  array = [...array]; //Creating a copy, so that we don't alter the original;
   const { sentenceOne, array: returnedArray } = await compareSentenceToArray(
     sentence,
-    array,
+    array
   );
 
   returnedArray.sort((a, b) => {
@@ -133,13 +219,46 @@ const arrayInOrder = async (sentence, array) => {
   };
 };
 
-function getProgress() {
-  if (!model) {
-    modelNotLoadedErrorMessage();
-    return;
-  }
+/**
+ * Returns the progress of the model loading process.
+ * 
+ * If the model is loading, it returns an Object that represents the progress of the model loading process.
+ * 
+ * @function
+ * @returns {Object} The progress of the model loading process.
+ * 
+ * @example
+ * try {
+ *   const progress = getProgress();
+ *   console.log(progress);
+ * } catch (error) {
+ *   console.error(error);
+ * }
+ */
+
+function getProgress() { 
   return progress;
 }
+
+/**
+ * Compares two sentences using the loaded model.
+ * 
+ * This function takes two sentences as input and uses the `classify` function to compare them. If the model has not been loaded, it throws an error.
+ * 
+ * @function
+ * @param {string} sentenceOne - The first sentence to compare.
+ * @param {string} sentenceTwo - The second sentence to compare.
+ * @returns {Object} The result object containing the two input sentences and the calculated similarity.
+ * @throws {Error} If the model has not been loaded.
+ * 
+ * @example
+ * try {
+ *   const result = compareTwoSentences("This is a sentence.", "This is another sentence.");
+ *   console.log(result);
+ * } catch (error) {
+ *   console.error(error);
+ * }
+ */
 
 function compareTwoSentences(sentenceOne, sentenceTwo) {
   if (!model) {
@@ -148,6 +267,17 @@ function compareTwoSentences(sentenceOne, sentenceTwo) {
   }
   return classify(sentenceOne, sentenceTwo);
 }
+
+/**
+ * The `vagueFinder` object provides a set of methods for comparing sentences using a loaded model.
+ * 
+ * @namespace
+ * @property {function} loadModel - Loads the model. See {@link loadModel}.
+ * @property {function} getProgress - Returns the progress of the model loading process. See {@link getProgress}.
+ * @property {function} compareTwoSentences - Compares two sentences using the loaded model. See {@link compareTwoSentences}.
+ * @property {function} compareSentenceToArray - Compares a sentence to an array of sentences using the loaded model. See {@link compareSentenceToArray}.
+ * @property {function} arrayInOrder - Compares a sentence to an array of sentences using the loaded model and returns the results in order of similarity. See {@link arrayInOrder}.
+ */
 
 const vagueFinder = {
   loadModel: loadModel,
